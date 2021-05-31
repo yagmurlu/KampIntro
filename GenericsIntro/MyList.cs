e60d4982ae2c2a194f56bc8cb843c4224effdc44
yagmurlu <mavibulut0106@gmail.com>
2021-05-31 23:57:59 +0300

    MyList

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    public class MyList<T>// buradki T verdiğimiz tipe den geliyor
+    {
+        T[] items;
+        //classı new"lersem oto. çalışır.
+        public MyList()//CTOR TAB TAB// constructor
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;//geçici dizi//elemanlar silinmesin diye
+            items = new T[items.Length + 1];//dizinin eleman sayısını  1 artır 
+            for (int i = 0; i < tempArray.Length; i++)
+                //items a eski değeleri geri verir
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;//eklenmek istenilen elemanı ekledim.
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            MyList<int> names2 = new MyList<int>();
+            names.Add("Aleyna");
+            names2.Add(10);
+        }
+    }
+}
