8963a338ee67dbbd71185b81f58adfd9a95c50d0
yagmurlu <mavibulut0106@gmail.com>
2021-06-04 02:49:08 +0300

    oop2

---
+++ OOP2/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string CustomerNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SirketAdı { get; set; }
+        public string TcNo { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
